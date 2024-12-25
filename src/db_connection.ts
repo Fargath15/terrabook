d81ec80479c149cd4983a1d7f31ef4fefed2ad1d
Fargath15 <manasfargath@gmail.com>
2024-12-25 21:13:33 +0530

    updated

--- src/db_connection.ts
+++ src/db_connection.ts
@@ -7,7 +7,7 @@ const dbHost: string = process.env.DB_HOST || "";
 const dbName: string = process.env.DB_NAME || "";
 const dbPass: string = process.env.DB_PASS || "";
 
-export const connection = mongoose.connect(("mongodb+srv://mongouser:mongopass@cluster0.fo3ix.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
+export const connection = mongoose.connect((`mongodb+srv://${dbName}:${dbPass}@${dbHost}/?retryWrites=true&w=majority&appName=Cluster0`))
     .then(res => {
         if (res) {
             console.log(`Database connection succeffully to ${dbName}`);
